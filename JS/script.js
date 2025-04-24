da732008be5861fabafc808c511f6e24dbd9c0c7
Nhu Trinh <nhutrinh6101997@gmail.com>
2025-04-25 02:14:15 +0700

    update folder CSS và JS as require

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,11 @@
+const wrapper = document.getElementById("slider-track");
+const nextBtn = document.getElementById("next-button");
+const prevBtn = document.getElementById("previous-button");
+let countNext = 0;
+
+/*
+Phương thức được sử dụng để ẩn title VNBookStore và hiện menu vnbookstore khi cuộn dọc cho home page
+*/
 window.addEventListener('scroll', function () {
     const header = document.getElementById('main-title');
     const menu = document.getElementById('vnbookstore');
@@ -9,14 +17,10 @@ window.addEventListener('scroll', function () {
       menu.style.display = 'none';
     }
 });
-  
-const wrapper = document.getElementById("slider-track");
-const nextBtn = document.getElementById("next-button");
-const prevBtn = document.getElementById("previous-button");
-let countNext = 0;
 
-
-
+/*
+Phương thức được sử dụng để thực hiện tác vụ cuộn ngang cho Our Bestsellers cho home page"
+*/
 const scrollAmount = wrapper.clientWidth;
 nextBtn.addEventListener("click", () => {
   wrapper.scrollBy({ left: scrollAmount, behavior: 'smooth' });
