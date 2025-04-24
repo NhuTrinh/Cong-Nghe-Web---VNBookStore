60ab917f9f10231e51e235f3d139ed4356ca961d
Nhu Trinh <nhutrinh6101997@gmail.com>
2025-04-25 01:35:43 +0700

    update scroll for home page

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('scroll', function () {
     const header = document.getElementById('main-title');
     const menu = document.getElementById('vnbookstore');
@@ -7,5 +6,39 @@ window.addEventListener('scroll', function () {
         menu.style.display = 'block'
     } else {
       header.style.display = 'block' 
+      menu.style.display = 'none';
     }
-  });
+});
+  
+const wrapper = document.getElementById("slider-track");
+const nextBtn = document.getElementById("next-button");
+const prevBtn = document.getElementById("previous-button");
+let countNext = 0;
+
+
+
+const scrollAmount = wrapper.clientWidth;
+nextBtn.addEventListener("click", () => {
+  wrapper.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  countNext++;
+  if (countNext > 0)
+  {
+    prevBtn.style.display = 'block';
+    nextBtn.style.display = 'none';
+  } else {
+    prevBtn.style.display = 'none'
+    nextBtn.style.display = 'block'
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  wrapper.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  countNext--;
+  if (countNext > 0)
+    {
+      prevBtn.style.display = 'block';
+    } else {
+    prevBtn.style.display = 'none'
+    nextBtn.style.display = 'block'
+    }
+});
